cd8377084a03a2b2df7ec783c75426c4c90989a5
Somo S <chisomo@mombo.solutions>
2023-06-15 16:19:01 +0000

    add examples
    Signed-off-by: Somo S <chisomo@mombo.solutions>

---
+++ Examples.lean
@@ -0,0 +1,52 @@
+import Supertype
+set_option pp.notation false
+
+
+-- section
+-- end
+#check {n // 5 + n < 17} -- Subtype (5 + · < 17)
+
+#check {n \\ 5 + n < 17} -- Supertype (5 + · < 17)
+
+open Notation.Override in 
+  #check {n // 5 + n < 17}  -- Supertype (5 + · < 17)
+
+
+----------
+#check {// 5 + · < 17} -- does not require notation override
+
+------------
+
+
+example : Supertype (5 + · < 17) := ... 2
+
+open Notation.Override in 
+  example : Supertype (5 + · < 17) := ↑2
+
+open Notation.Override in 
+  example : Supertype (5 + · < 17)  :=  ⟨2⟩ 
+  
+------------------------------------------------------------------------------------------
+
+---- ALT
+
+example : {n : Nat \\ n < 5} :=  {val := 2} 
+example : {n \\ n < 5} = Supertype ( · < 5) := rfl
+
+example : {\\ 3 + · ≤ 11} = Supertype (3 + · ≤ 11) := rfl  #check {\\ 3 + · ≤ 11}
+example : {\\ · < 5} := 2 ...
+example : {\\ · < 5} := ... 2
+
+
+-- OVERR
+section override 
+open Notation.Override 
+example : {n : Nat // n < 5} :=  {val := 2} 
+example : {n // n < 5} = Supertype ( · < 5) := rfl
+
+example : {// 3 + · ≤ 11} = Supertype (3 + · ≤ 11) := rfl  #check {// 3 + · ≤ 11}
+
+example : {// · < 5} :=  ↑2 
+example : {// · < 5} :=  ⟨ 2 ⟩
+example : {// · < 5} :=  ⟨ 2, by simp_arith ⟩
+end override
